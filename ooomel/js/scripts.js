fb50898ea0879292a854d70bb067631484a16015
sanns <iamthe@bk.ru>
2016-04-17 15:25:10 +0400

    like
    likeit (добавился person)
    и нажатие кнопки Like

--- ooomel/js/scripts.js
+++ ooomel/js/scripts.js
@@ -1,3 +1,16 @@
+//Console .log
+//
+SANZDEBUG = 1;
+
+function log1() {
+    if (SANZDEBUG == 1)
+        console.log(arguments);
+
+}
+
+
+
+
 var persons = [
      'Jhon'
     ,'Nill'
@@ -8,6 +21,7 @@ function CommentManager() {
     //
     // this = {}
     var commentsQnty_ = 0;
+    var commentsIncrement_ = 0; //не предназначен для уменьшения, в отличие от commentsQnty_;
     var comments = [ //:Comment[]
            
     ];
@@ -17,17 +31,22 @@ function CommentManager() {
         //
         // this = {}
         
-        this.id = commentsQnty_;
+        this.id = commentsIncrement_;
         this.parentId = _parent || 0;
         this.level = _lvl || 0;
         this.likesNum = 0;
+        this.personsLiked = {};
         
         this.person  = _person;     //строка?
         this.content = _content;    //строка?
         
         
-        this.likeIt = function(){
-            ++this.likesNum;
+        
+        this.likeIt = function(_person , undefined){//тот, кто лайкает, а не создает комент
+            if (this.personsLiked[_person] === undefined) {
+                this.personsLiked[_person] = true;
+                ++this.likesNum;
+            }
         }
         
         this.response = function() {
@@ -42,6 +61,7 @@ function CommentManager() {
     
     
     this.newComment = function( _person, _content, _lvl , _parent){
+        if (_content == '') return;
         
         //validation:
         if (_lvl === 0) _parent = 0;
@@ -59,11 +79,31 @@ function CommentManager() {
         comments.push( new Comment(_person, _content, _lvl, _parent) );
         
         ++commentsQnty_;
+        ++commentsIncrement_;
     }
     this.getComment = function(_comment_idx) {
+        //по индексу в массиве
         
         return comments[_comment_idx];
         
+    }
+    this.getCommentById = function(_comment_id) {
+        //вернет комментарий по id , или undefined
+        _comment_id*=1;
+        
+        
+        for( var i=0, l=comments.length; i<l; ++i ) {
+            if( comments[i].id  === _comment_id ){
+                
+                var commentTemp = comments[i];
+                
+                
+                
+            }
+
+        }
+        
+        return commentTemp;
     }
     this.consoleComments = function(_cons_tag) {
         console.log(_cons_tag, commentsQnty_ );
@@ -71,9 +111,9 @@ function CommentManager() {
     }
     this.drawComments = function() {
         $(document).ready(function(){
+            $('body').empty();
                           
             $('<p>').text('Posts : '+ commentsQnty_).appendTo('body');
-            $('<textarea>').appendTo('body');
             
             //document.write('<style> td { color: #F40 } </style>');
             
@@ -91,7 +131,9 @@ function CommentManager() {
                     //разница?!
                     //$('<div>').appendTo('body').text(commentTemp.content);
                     $('<div>')
-                        .attr('data-id' , commentTemp.id ).text(commentTemp.content)
+                        .addClass('comment').attr('data-id' , commentTemp.id )
+                        .append('<div class="person">'+commentTemp.person)
+                        .append('<span>'+commentTemp.content)
                         .append('<div class="likes_qnty">'+commentTemp.likesNum)
                         .append('<button class="likeit">Like')
                     
@@ -114,7 +156,9 @@ function CommentManager() {
                     //разница?!
                     //$('<div>').appendTo('body').text(commentTemp.content);
                     $('<div>')
-                        .attr('data-id' , commentTemp.id ).text(commentTemp.content)
+                        .addClass('comment').attr('data-id' , commentTemp.id )
+                        .append('<div class="person">'+commentTemp.person)
+                        .append('<span>'+commentTemp.content)
                         .append('<div class="likes_qnty">'+commentTemp.likesNum)
                         .append('<button class="likeit">Like')
                         
@@ -124,6 +168,9 @@ function CommentManager() {
                 }
                 
             }
+            
+            $('<textarea>').appendTo('body');
+            $('<button class="send_cmnt">Send comment</button>').appendTo('body');
             
         });
     }
@@ -137,8 +184,8 @@ function CommentManager() {
 var commentManager1 = new CommentManager();
 
 commentManager1.newComment(persons[0] , 'Content#1: lallal.');
-commentManager1.newComment(persons[1] , 'Content#2: lallal.' , 1 , 0);
-commentManager1.newComment(persons[1] , 'Content#3: lallal.');
+commentManager1.newComment(persons[1] , 'Content#2: lallal.');
+commentManager1.newComment(persons[1] , 'Content#3: lallal.' , 1 , 0);
 commentManager1.newComment(persons[0] , 'Content#4: lallal.');
 
 
@@ -146,10 +193,10 @@ var comment1 = commentManager1.getComment(1);
 
 console.log(comment1);
 
-comment1.likeIt();
-comment1.likeIt();
-comment1.likeIt();
-comment1.likeIt();
+comment1.likeIt(persons[1]);
+comment1.likeIt(persons[1]);
+comment1.likeIt(persons[1]);
+comment1.likeIt(persons[1]);
 
 console.log(comment1);
 
@@ -173,9 +220,28 @@ commentManager2.consoleComments(2);
 $(document).ready(function(){
     
 });
+$(document).on('click', '.likeit', function() {
+    var $thisClkLk = $(this);
+    
+    var commentClicked$ = $thisClkLk.closest('.comment')
+    , commentClickedId = commentClicked$.attr('data-id')
+    , commentClickedObj = commentManager1.getCommentById(commentClickedId);
+    
+    commentClickedObj.likeIt(persons[0]);
+    commentManager1.drawComments(1);
+    
+});
 $(document).on('click', '.response', function() {
     var $thisClkRsp = $(this);
-    
-    
+});
 
+$(document).on('click', '.send_cmnt', function() {
+    var $thisClkSend = $(this);
+    
+    var textarea = $('textarea'),
+        commentText = textarea.val();
+    
+    
+    commentManager1.newComment(persons[0] , commentText);
+    commentManager1.drawComments(1);
 });
